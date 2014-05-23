f8f06e72ae6d6afbb7c8a56210ba5d17abb3272e
Anthony Garza <agarza1972@yahoo.com>
2014-05-23 11:17:35 -0500

    Added form that allows user to send an email

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -8,6 +8,7 @@
 		var_dump($_GET);
 		var_dump($_POST);
     ?>
+    <h1>User Login</h1>
 	<form method="POST" action="my_first_form.php">
 	    <p>
 	        <label for="username">Username</label>
@@ -20,6 +21,27 @@
 	    <p>
 	        <button type="submit">Login</button>
 	    </p>
-</form>
+	</form>
+	<h1>Compose an Email</h1>
+	<form>
+		<p>
+		    <label for="from_email">Senders Email Address</label>
+	        <input id="from_email" name="from_email" type="email" placeholder="Enter senders email">
+		</p>
+		<p>
+            <label for="to_email">Recipients Email Address</label>
+	        <input id="to_email" name="to_email" type="email" placeholder="Enter recipients email">
+		</p>
+		<p>
+		    <label for="subject">SUBJECT</label>
+	        <input id="subject" name="subject" type="text" placeholder="Enter SUBJECT">
+		</p>
+		<p>
+			<textarea id="email_body" name="email_body" rows="5" cols="120">Email Content Here</textarea>
+		</p>
+		<p>
+	        <button type="submit">SEND</button>
+	    </p>
+	</form>
 </body>
 </html>
